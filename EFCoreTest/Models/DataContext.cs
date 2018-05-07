1111dc8ceebcdeb31614b1d5a4523bebe5c18e46
Brian Futrell <bfutrell@stayonline.com>
2018-05-07 13:47:49 -0400

    data context using InMemory provider

---
+++ EFCoreTest/Models/DataContext.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EFCoreTest.Models
+{
+    public class DataContext : DbContext
+    {
+        public DataContext() : base()
+        { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseInMemoryDatabase("InMemoryTest");
+            }
+        }
+
+        public DbSet<Plug> Plugs { get; set; }
+        public DbSet<Connector> Connectors { get; set; }
+        public DbSet<Cord> Cords { get; set; }
+        public DbSet<Lookup> Lookups { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Plug>().ToTable("Plugs").HasKey("PlugId");
+            modelBuilder.Entity<Connector>().ToTable("Connectors").HasKey("ConnectorId");
+            modelBuilder.Entity<Cord>().ToTable("Cords").HasKey("CordId");
+
+            modelBuilder.Entity<Lookup>().ToTable("Lookups").HasKey("Sku");
+
+            // relationships between lookup and plug/connector/cord
+            modelBuilder.Entity<Lookup>().HasMany(x => x.Plugs).WithOne(x => x.Lookup).HasForeignKey(f => f.Sku).HasPrincipalKey(p => p.Sku);
+            modelBuilder.Entity<Lookup>().HasMany(x => x.Connectors).WithOne(x => x.Lookup).HasForeignKey(f => f.Sku).HasPrincipalKey(p => p.Sku);
+            modelBuilder.Entity<Lookup>().HasMany(x => x.Cords).WithOne(x => x.Lookup).HasForeignKey(f => f.Sku).HasPrincipalKey(p => p.Sku);
+        }
+    }
+}
